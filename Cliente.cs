34c2ed6f1ec01bdbf181586b9a510c3b4abbf57f
Leticia P <112350909+leti404@users.noreply.github.com>
2024-05-06 08:19:04 -0300

    creacion cliente

---
+++ Cliente.cs
@@ -0,0 +1,17 @@
+class Cliente {
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public int DNI { get; private set; } 
+        public double FechaInscripcion { get; set; }
+        public int TipoEntrada { get; set; }
+        public int Cantidad { get; set; }
+        public Cliente(string nombre, string apellido, int dni, double fechaInscripcion, int TipoEntrada, int cantidad) 
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            DNI = dni;
+            FechaInscripcion = fechaInscripcion;
+            TipoEntrada = tipoEntrada;
+            Camtidad = cantidad;
+        }   
+}
