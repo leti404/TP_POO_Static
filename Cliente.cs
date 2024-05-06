69c576fa04ee41699084ce870be9c37ab9a23dcf
Leticia P <112350909+leti404@users.noreply.github.com>
2024-05-06 09:00:39 -0300

    creacion do, funciones basicas y case 1

--- Cliente.cs
+++ Cliente.cs
@@ -2,10 +2,10 @@ class Cliente {
         public string Nombre { get; private set; }
         public string Apellido { get; private set; }
         public int DNI { get; private set; } 
-        public double FechaInscripcion { get; set; }
+        public Date FechaInscripcion { get; set; }
         public int TipoEntrada { get; set; }
         public int Cantidad { get; set; }
-        public Cliente(string nombre, string apellido, int dni, double fechaInscripcion, int TipoEntrada, int cantidad) 
+        public Cliente(string nombre, string apellido, int dni, Date fechaInscripcion, int TipoEntrada, int cantidad) 
         {
             Nombre = nombre;
             Apellido = apellido;
--- Program.cs
+++ Program.cs
@@ -1,8 +1,61 @@
 using System;
-class TP_POO_static
+internal class Program
 {
+    Cliente cliente1 = null;
     static void Main(string[] args)
     {
+        const int OP1 = 1, OP2 = 2, OP3 = 3, OP4 = 4, OP5 = 5;
+        int opcion;
+        
+        do
+        {
+            opcion = IngresarEnteroConParametros("1. Nueva Inscripción, 2. Obtener Estadísticas del Evento, 3. Buscar Cliente, 4. Cambiar entrada de un Cliente, 5.Salir", OP1, OP5);
+            switch (opcion)
+            {
+                case OP1:
+                    cliente1 = ObtenerCliente();
+                    Console.WriteLine($"Se ha creado el superheroe {superheroe1.Nombre}");
+                break;
+
+                case OP2:
+                break;
+                case OP3:
+                break;
+            }
+
+        }while(opcion != OP5);
 
     }
+    static int IngresarEnteroConParametros(string mensaje, int max, int min)
+    {
+        int numero;
+        Console.WriteLine(mensaje);
+        while (!int.TryParse(Console.ReadLine(), out numero) || numero <= max && numero > min)
+        {
+            Console.WriteLine("Error, ingrese un número válido");
+        }
+        return numero;
+    }
+    static string IngresoCadena(string mensaje)
+    {
+        string cadena;
+        Console.WriteLine(mensaje);
+        do 
+        {
+            cadena = Console.ReadLine();
+        }while(cadena == null);
+        return cadena;
+    }
+    static Cliente ObtenerCliente()
+    {
+        string nombre = IngresoCadena("Ingrese el nombre");
+        string apellido= IngresoCadena("Ingrese el Apellido");
+        int dni = IngresarEnteroConParametros("Ingrese el DNI", 100000000, 0);
+        int TipoEntrada = IngresarEnteroConParametros("Ingrese el tipo de entrada", 4, 0);
+        double fechaInscripcion = date.Today;
+        Cliente cliente1 = new Cliente(nombre, apellido, dni, );
+        return cliente1;
+    }
+
+
 }
